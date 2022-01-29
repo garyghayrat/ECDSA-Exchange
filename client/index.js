dd9eb1e0757f20f2e9e5aeb671344533821f2a07
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-01-29 14:57:29 -0500

    completed project

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 import "./index.scss";
+const SHA256 = require('crypto-js/sha256');
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
 
 const server = "http://localhost:3042";
 
@@ -19,9 +22,13 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("private-key").value;
+
+  var checkHash = SHA256(sender).toString();
+  var signature = ec.keyFromPrivate(privateKey, 'hex').sign(checkHash);
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, signature
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
--- server/index.js
+++ server/index.js
@@ -3,16 +3,42 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
+const SHA256 = require('crypto-js/sha256');
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+
+const key1 = ec.genKeyPair();
+const key2 = ec.genKeyPair();
+const key3 = ec.genKeyPair();
+
+const publicKey1 = key1.getPublic().encode('hex');
+const publicKey2 = key2.getPublic().encode('hex');
+const publicKey3 = key3.getPublic().encode('hex');
+
+const privateKey1 = key1.getPrivate().toString(16);
+const privateKey2 = key2.getPrivate().toString(16);
+const privateKey3 = key3.getPrivate().toString(16);
+
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+  [publicKey1]: 100,
+  [publicKey2]: 50,
+  [publicKey3]: 75,
 }
+
+console.log("Address 1 public key: " + publicKey1 + " with balance of " + balances[publicKey1] + " ETH");
+console.log("Address 2 public key: " + publicKey2 + " with balance of " + balances[publicKey2] + " ETH");
+console.log("Address 3 public key: " + publicKey3 + " with balance of " + balances[publicKey3] + " ETH");
+
+console.log("");
+
+console.log("Address 1 private key: " + privateKey1);
+console.log("Address 2 private key: " + privateKey2);
+console.log("Address 3 private key: " + privateKey3);
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -21,9 +47,19 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
+  const {sender, recipient, amount, signature} = req.body;
+
+  var checkHash = SHA256(sender).toString();
+  console.log(signature);
+  console.log(ec.keyFromPublic(sender, 'hex').verify(checkHash, signature));
+
+  if(ec.keyFromPublic(sender, 'hex').verify(checkHash, signature)) {
+    
   balances[sender] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
+
+  }
+
   res.send({ balance: balances[sender] });
 });
 
